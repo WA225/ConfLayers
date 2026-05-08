d5c86cf737adaa2f79e927c370bb4d2a3d320af7
WA225 <walaaa@uci.edu>
2026-05-08 10:30:45 -0700

    Clean up eval.sh
    Removed SWIFT hyperparameters and related inference calls.

--- eval.sh
+++ eval.sh
@@ -21,19 +21,3 @@ LAMBDA=0.25
 OPT_METHOD="dynamic"
 
 CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m evaluation.inference_conflayer --model-path $MODEL_PATH --model-id ${MODEL_NAME} --temperature $TEMP --top-p ${TOP_P} --dtype $torch_dtype --task-name ${TASK_NAME} --data-num ${DATA_NUM} --max-new-tokens ${MAX_NEW_TOKENS} --seed $SEED --context-window ${CONTEXT_WINDOW} --opt-interval ${OPT_INTERVAL} --bayes-interval ${BAYES_INTERVAL} --max-opt-iter ${MAX_OPT_ITER} --max-tolerance-iter ${MAX_TOLERANCE_ITER} --max-score ${MAX_SCORE} --skip-ratio ${SKIP_RATIO} --optimization ${OPT_METHOD} --bayes --lam ${LAMBDA} --cache-hit &> logs/conflayers.out
-
-
-# SWIFT Hyperparameters
-SWIFT_OPT_INTERVAL=1
-SWIFT_BAYES_INTERVAL=25
-SWIFT_MAX_OPT_ITER=1000
-SWIFT_MAX_TOLERANCE_ITER=300
-SWIFT_MAX_SCORE=0.95
-SWIFT_CONTEXT_WINDOW=32
-SWIFT_SKIP_RATIO=0.45
-OPT_METHOD="swift"
-
-CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m evaluation.inference_baseline --model-path $MODEL_PATH --model-id ${MODEL_NAME} --max-new-tokens ${MAX_NEW_TOKENS} --task-name ${TASK_NAME} --data-num ${DATA_NUM} --temperature $TEMP --top-p ${TOP_P} --seed ${SEED} --dtype $torch_dtype &> logs/vanilla.out
-
-CUDA_VISIBLE_DEVICES=${GPU_DEVICES} python -m evaluation.inference_conflayer --model-path $MODEL_PATH --model-id ${MODEL_NAME}  --temperature $TEMP --top-p ${TOP_P} --dtype $torch_dtype --task-name ${TASK_NAME} --data-num ${DATA_NUM} --max-new-tokens ${MAX_NEW_TOKENS} --seed $SEED --context-window ${SWIFT_CONTEXT_WINDOW} --opt-interval ${SWIFT_OPT_INTERVAL} --bayes-interval ${SWIFT_BAYES_INTERVAL} --max-opt-iter ${SWIFT_MAX_OPT_ITER} --max-tolerance-iter ${SWIFT_MAX_TOLERANCE_ITER} --max-score ${SWIFT_MAX_SCORE} --skip-ratio ${SWIFT_SKIP_RATIO} --optimization ${OPT_METHOD} --bayes --cache-hit &> logs/swift.out
-
